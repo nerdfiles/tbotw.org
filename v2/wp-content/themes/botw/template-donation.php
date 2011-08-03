e4e7a793b2d9f87702b1d2c66837d00ce9d4bfcc
Aaron Alexander <nerdfiles@gmail.com>
2011-08-03 17:09:23 -0500

    cleanup

--- v2/wp-content/themes/botw/template-donation.php
+++ v2/wp-content/themes/botw/template-donation.php
@@ -18,32 +18,6 @@ get_header(); ?>
 
 					<?php the_content(); ?>
 
-<!-- Begin Hardcoded Template -->
-<div class="cushycms-wysiwyg" title="Donation Form">
-    <form id="donation-form" method="post" action="https://www.eProcessingNetwork.com/cgi-bin/wo/order.pl">
-    
-        <h2>Make a Donation</h2>
-        
-        <fieldset>
-        <input type="hidden" name="ePNAccount" value="1010312" />
-        <input type="hidden" name="URL" value="http://www.thebridgeovertroubledwaters.org/v2/donate/" />
-        <input type="hidden" name="ItemDesc" value="Donation" />
-        <input type="hidden" name="ItemQty" maxlength="3" value="1" />
-            <legend class="position_zap">Enter a Donation Amount</legend>
-            <dl class="clearfix no_margin">
-                <dt class="grid_3 alpha"><label for="ItemCost">Donation Amount</label></dt>
-                <dd class="grid_5 omega basement_2"><input type="text" id="ItemCost" name="ItemCost" value="0" tabindex="1" /></dd>
-            </dl>
-            <div class="push_3">
-                <input type="submit" class="submit" value="Donate" tabindex="2" />
-            </div>
-        </fieldset>
-    
-    </form>
-</div>
-
-<!-- End Hardcoded Template -->
-
 				<?php hybrid_after_entry(); // After entry hook ?>
 
 			<?php hybrid_after_singular(); // After singular hook ?>
